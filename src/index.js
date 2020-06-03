b300c87467bef771da23c9e86e9729334db48d52
David Smith <daSmith1105@gmail.com>
2020-06-03 12:30:04 -0500

    changed router type from Browser Router to Router. history.push(/live) was not working with browser router after successful login. this change resolves that issue

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import history from './history';
 
 ReactDOM.render(
